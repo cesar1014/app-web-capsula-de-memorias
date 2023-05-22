3f97a3150596004c719bc036d13629130f6dada5
unknown <xcesaryt@gmail.com>
2023-05-22 16:28:12 -0300

    first commit

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      🚀Feito Por{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+        href="https://github.com/cesar1014"
+      >
+        Cesar Santana
+      </a>
+    </div>
+  )
+}
